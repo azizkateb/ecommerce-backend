ffd02af73c936bee2344309fde1e2bcec10ddb1e
azizelkateb <azizkateb69@gmail.com>
2026-05-10 12:00:52 +0100

    fix CORS for production frontend

--- src/main/java/com/shop/crud/Config/SecurityConfig.java
+++ src/main/java/com/shop/crud/Config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:4200", "http://localhost:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:4200","https://azizkateb.github.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
